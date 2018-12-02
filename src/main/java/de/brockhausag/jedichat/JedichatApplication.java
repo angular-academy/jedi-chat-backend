0741b3ba695a7b424548cbc68b2e931fc04d221f
bzmmarvin222 <bzmmarvin222@gmail.com>
2018-12-02 13:18:56 +0100

    init

---
+++ src/main/java/de/brockhausag/jedichat/JedichatApplication.java
@@ -0,0 +1,12 @@
+package de.brockhausag.jedichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedichatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JedichatApplication.class, args);
+	}
+}
---
+++ src/main/java/de/brockhausag/jedichat/controllers/ProfileController.java
@@ -0,0 +1,7 @@
+package de.brockhausag.jedichat.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api/profile")
+public class ProfileController {
+}
